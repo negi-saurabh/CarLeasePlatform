fa6d68776bcf6f3c9b1a204259352431205cc549
negi-saurabh <saurabh.negi89@gmail.com>
2022-07-10 17:26:31 +0200

    Initial Boiler Plate

---
+++ car-lease/src/main/java/com/sogeti/carlease/CarLeaseApplication.java
@@ -0,0 +1,13 @@
+package com.sogeti.carlease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLeaseApplication.class, args);
+	}
+
+}
