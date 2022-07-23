a0409a5e8e1b96064a27b3b449efc008302c820e
negi-saurabh <saurabh.negi89@gmail.com>
2022-07-23 16:01:21 +0200

    Role based access

--- car-lease/src/main/java/com/sogeti/carlease/controllers/BrokerController.java
+++ car-lease/src/main/java/com/sogeti/carlease/controllers/BrokerController.java
@@ -3,6 +3,7 @@ package com.sogeti.carlease.controllers;
 import com.sogeti.carlease.models.Customer;
 import com.sogeti.carlease.services.BrokerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
@@ -10,39 +11,46 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/customer")
 public class BrokerController {
 
     @Autowired
     private BrokerService brokerService;
 
     @GetMapping(value="/all")
+    @PreAuthorize("hasAuthority('BROKER')")
     public List<Customer> readAll() {
         return brokerService.getAllCustomers();
     }
 
     @GetMapping(value="/{id}")
+    @PreAuthorize("hasAuthority('BROKER')")
     public List<Customer> read(@PathVariable int id) {
         Optional<Customer> customer = brokerService.findById(id);
         return customer.map(List::of).orElse(Collections.emptyList());
     }
 
     @PostMapping(value="/addNew")
+    @PreAuthorize("hasAuthority('BROKER')")
     public Customer create(@RequestBody final Customer customer) {
         return brokerService.createCustomer(customer);
     }
 
     @RequestMapping(value= "/update/{id}", method = RequestMethod.PUT)
+    @PreAuthorize("hasAuthority('BROKER')")
     public Customer update(@PathVariable int id, @RequestBody Customer customer){
         return brokerService.updateCustomer(customer, id);
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    @PreAuthorize("hasAuthority('BROKER')")
     public void delete(@PathVariable int id){
         //need to check children records before deleting
         brokerService.deleteCustomer(id);
     }
 
     @RequestMapping(value="/calculateLease", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('BROKER')")
     public double calculateLease(@RequestParam(value = "Mileage" , required = true) double mileage,
                                  @RequestParam(value = "Duration", required = true) double duration,
                                  @RequestParam(value = "InterestRate", required = true) double interestRate,
--- car-lease/src/main/java/com/sogeti/carlease/controllers/CarController.java
+++ car-lease/src/main/java/com/sogeti/carlease/controllers/CarController.java
@@ -3,6 +3,7 @@ package com.sogeti.carlease.controllers;
 import com.sogeti.carlease.models.Car;
 import com.sogeti.carlease.services.CarLeaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.bind.ValidationException;
@@ -17,17 +18,20 @@ public class CarController {
     private CarLeaseService carLeaseService;
 
     @GetMapping(value="/all")
+    @PreAuthorize("hasAuthority('EMPLOYEE')")
     public List<Car> readAll() {
         return carLeaseService.getAllCars();
     }
 
     @GetMapping(value="/{id}")
+    @PreAuthorize("hasAuthority('EMPLOYEE')")
     public Car read(@PathVariable int id) {
         Optional<Car> car = carLeaseService.findById(id);
         return car.stream().findFirst().orElse(null);
     }
 
     @PostMapping(value="/addNew")
+    @PreAuthorize("hasAuthority('EMPLOYEE')")
     public Car create(@RequestBody final Car car) throws ValidationException {
         if(car.getMake() != null && car.getModel() != null && car.getNettPrice() != 0.0)
             return carLeaseService.createCar(car);
@@ -36,6 +40,7 @@ public class CarController {
     }
 
     @RequestMapping(value="/update/{id}", method = RequestMethod.PUT)
+    @PreAuthorize("hasAuthority('EMPLOYEE')")
     public Car update(@PathVariable int id, @RequestBody Car car) throws ValidationException {
         if(carLeaseService.findById(id).isPresent()) {
             return carLeaseService.updateCar(car, id);
@@ -45,11 +50,9 @@ public class CarController {
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    @PreAuthorize("hasAuthority('EMPLOYEE')")
     public void delete(@PathVariable int id){
         //need to check children records before deleting
         carLeaseService.deleteCar(id);
     }
-
-
-
 }
